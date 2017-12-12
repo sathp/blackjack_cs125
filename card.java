fbb32694c555c1b15151a4789a004074d1d169e1
Satwik Singh <satwik@wirelessprv-10-194-21-114.near.illinois.edu>
2017-12-11 20:00:49 -0600

    fixed player and card

--- card.java
+++ card.java
@@ -3,8 +3,12 @@ class Card{
 	private int value;
 	private String name;
 	public Card(){}
-	public Card(String name1, value){
+	public Card(String name1, int value1){
 		this.name = new String(name1);
+		this.value = value1;
+	}
+	public String getName() {
+		return this.name;
 	}
 	public int getVal(){
 		return this.value;
