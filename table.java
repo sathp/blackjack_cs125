706192758d368da3699ea733e0e65434f1e7870d
Sathwik Pochampally <sathwik@outlook.com>
2017-12-12 02:37:18 -0600

    Minor Changes

--- table.java
+++ table.java
@@ -1,3 +1,8 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+import player.java;
+
 class Table{
 
     /* Class variable that holds multiple number of decks of cards. */
@@ -12,22 +17,24 @@ class Table{
      */
     private int playersStanding; // How many players are 'stand'ing. When all the players are standing, the dealer's cards are displayed.
 
+    /* Class variable that holds the minimum bet amount required to play a game. */
+    private double minBet
     /*
      * Creates a new table with the given parameters.
      * Only for starting a game from fresh.
      * @param numOfPlayers - Number of players playing the game.
      * @param money - Amount of money each player starts with.
      */
-    public Table(final int numOfPlayers, final double money) {
+    public Table(final int numOfPlayers, final double money, final double minBet) {
 
         // Makes an array of decks
-        decks = new Deck[4];
-        for (i = 0; i < decks.length; i++) {
-            decks[i] = new Deck;
-        }
+        this.newDeck();
 
         // Creates a new Dealer object.
-        dealer = new Dealer(money);
+        this.dealer = new Dealer(money);
+
+        // Sets the minimum bet to play a game at the table.
+        this.minBet = minBet;
 
         // Creates an array of players and gives them money.
         players = new Player[numOfPlayers];
@@ -37,5 +44,27 @@ class Table{
 
         // Keeps of track of the number of players standing. Updates every turn.
         int playersStanding = 0;
+    }
+
+    /*
+     * Used to start a new game once a game has been won and available balance is greater
+     * than the minimum amount needed for each game.
+     */
+    public void newGame() {
+        this.newDeck();
+        for (i = 0; i < players.length - 1; i++) {
+            players[i].hand = ArrayList<Card>();
+        }
+    }
+
+    public void newDeck() {
+        this.decks = new Deck[4];
+        for (i = 0; i < decks.length; i++) {
+            this.decks[i] = new Deck;
+        }
+    }
+
+    public void deal() {
+        
     }
 }
