5099a6c8abef56902fe80cc7290ea542e62e1df3
Satwik Singh <satwik@Satwiks-MacBook-Pro.local>
2017-12-11 22:10:33 -0600

    Rip

--- card.java
+++ card.java
@@ -2,15 +2,27 @@ class Card{
 
 	private int value;
 	private String name;
+	private int suit;
+	private boolean inDeck = true;
 	public Card(){}
-	public Card(String name1, int value1){
+	public Card(String name1, int value1, int value2){
 		this.name = new String(name1);
 		this.value = value1;
+		this.suit = value2;
 	}
 	public String getName() {
 		return this.name;
 	}
 	public int getVal(){
 		return this.value;
+	}
+	public int getSuit() {
+		return this.suit;
+	}
+	public boolean getStatus() {
+		return inDeck;
+	}
+	public void setStatus(boolean b) {
+		inDeck = b;
 	}
 }
---
+++ deck.java
@@ -0,0 +1,7 @@
+class Deck{
+	private Card[] deck;
+	public Deck(){
+		deck = new Card[52];
+		for(int i = 0; i )
+	}
+}
